3affbbbdbe3bd47229d998496b8a73d2851e3972
ketakirk <ketaki.kekatpure@gmail.com>
2015-09-10 08:50:19 -0700

    Modified nutriapp.js file

--- app/static/js/nutriapp.js
+++ app/static/js/nutriapp.js
@@ -12,11 +12,21 @@ $(document).ready(function(){
 	// Handler for read event
 	$("#readprofbtn").click(function(){
 		var fid = $("#readprofinput").val();
+		// alert(fid)
 		url = globals.baseUrl + "/profile/" + fid.toString();
 		$.getJSON(url, function (data) {
-			newHtml = "<h3> First name =" 
-					  + data["profile_data"]["first_name"]
-					  + "</h3>"			
+			newHtml = "<h4> Food item =" 
+					  + data["food_data"]["display_name"]
+					  + "</h4>"	
+					  + "<p> Portion size: "
+					  + data["food_data"]["portion_amt"]
+					  +"</p>"
+					  + "<p> Calories: "
+					  + data["food_data"]["calories"]
+					  +"</p>"
+					  + "<p> Added Sugars: "
+					  + data["food_data"]["added_sugars"]
+					  +"</p>"		
 			// alert(newHtml);
 			panelContent = $("#panelcontent");
 			// alert(panelContent.text());
